a49ab9e63d5dee390a58bba4cfd74320b1992b19
Guilherme Libardi <guilhermelibardi@hotmail.com>
2023-10-25 01:58:01 -0300

    added GUNICORN server

---
+++ server.py
@@ -0,0 +1,23 @@
+from gunicorn.app.base import BaseApplication
+from app import app
+
+class GUnicornFlaskApplication(BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        for key, value in self.options.items():
+            if key in self.cfg.settings and value is not None:
+                self.cfg.set(key, value)
+
+    def load(self):
+        return self.application
+    
+if __name__ == '__main__':
+    options = {
+        'bind': '0.0.0.0:50135',
+        'workers': 4,
+    }
+    GUnicornFlaskApplication(app, options).run()
